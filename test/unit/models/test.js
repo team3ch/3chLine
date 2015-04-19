2ea3d0e81403f656a57fb1a84c8030148bbd3f1b
hkdnet <hkdnet@users.noreply.github.com>
2015-04-20 02:12:05 +0900

    change test description

--- test/unit/models/test.js
+++ test/unit/models/test.js
@@ -1,13 +1,13 @@
 var assert = require('assert');
 
 describe.only('User', function() {
-  describe('#find()', function() {
-    it('user should have his/her id', function(done) {
+  describe('#id', function() {
+    it('should be numeric', function(done) {
       User.find().limit(1).then(function(data) {
         user = data[0]
         assert.equal(user.id, parseInt(user.id, 10))
         done();
       }).catch(done)
-    })
+    });
   })
 })
