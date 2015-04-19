cede29cd011332714b31b0bffb4db063e4e121c8
hkdnet <hkdnet@users.noreply.github.com>
2015-04-19 18:07:56 +0900

    a sample of get tweets and their user

--- api/controllers/TweetController.js
+++ api/controllers/TweetController.js
@@ -6,6 +6,33 @@
  */
 
 module.exports = {
-	
+	withUser : function(req, res) {
+		var users = {};
+		var promises = [];
+		var ret;
+		Tweet.find().exec(function(err, tweets) {
+			ret = tweets;
+			tweets.forEach(function(t) {
+				var id = t.user;
+				if(!users[id]){
+					users[id] = true
+					promises.push(User.findOne(id).then(function(user) {
+						users[id] = user
+					}));
+				}
+			})
+			Promise.all(promises).then(function() {
+				ret.map(function(t) {
+					t.user = users[t.user]
+					return t;
+				});
+				res.json(ret)
+			});
+		});
+	},
+	withUser2: function(req, res) {
+		Tweet.find().populate('user').exec(function(err, data) {
+			res.json(data)
+		})
+	}
 };
-
