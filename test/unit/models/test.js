f1e3a11063c5562af9008445e1ee2f73efd94222
hkdnet <hkdnet@users.noreply.github.com>
2015-04-24 02:14:44 +0900

    add userId test case

--- test/unit/models/test.js
+++ test/unit/models/test.js
@@ -1,21 +1,62 @@
 var assert = require('assert');
 
-describe.only('User', function() {
-  describe('#id', function() {
-
-    before(function(done) {
-      User.create({
-        userId: 'zoi',
-        username: 'aoba'
-      }).exec(done);
-    })
-
-    it('should be numeric', function(done) {
+describe('User', function() {
+  var user1;
+  before(function(done) {
+    User.create({
+      userId: 'zoi',
+      username: 'aoba'
+    }).exec(function() {
       User.find().limit(1).then(function(data) {
-        var user = data[0]
-        assert.strictEqual(user.id, parseInt(user.id, 10))
+        user1 = data[0]
         done();
       }).catch(done)
     });
+  })
+
+  describe('#id', function() {
+    it('should be numeric', function() {
+      assert(typeof user1.id === 'number')
+    });
+    it('should be intager', function() {
+      assert.strictEqual(user1.id, parseInt(user1.id, 10))
+    })
+  })
+  describe('#userId', function() {
+    it('should be string', function() {
+      assert(typeof user1.username === 'string')
+    })
+    describe('should be alphanumericdashed', function() {
+      it('should accept number-only userId', function(done) {
+        User.create({
+          userId: '0123456798',
+          username: 'only number'
+        }).exec(done)
+      });
+      it('should accept alphabet-only userId', function(done) {
+        User.create({
+          userId: 'abcdefghijklmnopqrstuvwxyz',
+          username: 'only alphabet'
+        }).exec(done)
+      });
+      it('should accept dash-only userId', function(done) {
+        User.create({
+          userId: '-_',
+          username:"hyphen and underscore"
+        }).exec(done)
+      });
+      it('should not accept comma', function(done) {
+        User.create({
+          userId: ',',
+          username: 'comma'
+        }).then(function() {
+          assert.fail();
+        }, function() {
+          assert(true)
+        }).finally(function() {
+          done();
+        });
+      });
+    })
   })
 })
