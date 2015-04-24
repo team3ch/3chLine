bb15c032d2e7b032ab4303b9286c6c7f994a01bf
hkdnet <hkdnet@users.noreply.github.com>
2015-04-25 03:10:12 +0900

    model should have maxLength property instead of size, because size is a database limit

--- api/models/User.js
+++ api/models/User.js
@@ -18,12 +18,14 @@ module.exports = {
       type: 'alphanumericdashed',
       unique: true,
       size: 20,
+      maxLength: 20,
       required: true
     },
 
     username: {
       type: 'string',
       size: 128,
+      maxLength: 128,
       required: true
     },
 
